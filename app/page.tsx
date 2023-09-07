be73bc16e8b649b8feb405b885441dc1a379e709
devlopersabbir <devlopersabbir@gmail.com>
2023-09-07 20:28:21 +0600

    Comming soon Page

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="flex w-full h-[100vh] justify-center items-center">
+      <h1 className="flex justify-center items-center text-2xl font-semibold">
+        Comming soon
+      </h1>
     </div>
   );
 };
