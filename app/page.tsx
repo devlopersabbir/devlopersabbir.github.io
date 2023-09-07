0b3bcdd194604b6b1e74d98fdd33c19068a62d7f
devlopersabbir <devlopersabbir@gmail.com>
2023-09-07 20:09:56 +0600

    Init

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-const page = () => {
-  return <div>page</div>;
+const Home = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
 };
 
-export default page;
+export default Home;
