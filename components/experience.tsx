67d7cc7afc3a8e771a7f12442a6b05ff0bb92246
devlopersabbir <devlopersabbir@gmail.com>
2023-11-29 16:26:32 +0600

    Fix: Depoly issue

--- components/experience.tsx
+++ components/experience.tsx
@@ -18,7 +18,7 @@ export default function Experience() {
   return (
     <section id="experience" ref={ref} className="scroll-mt-28 mb-28 sm:mb-40">
       <SectionHeading>My experience</SectionHeading>
-      <VerticalTimeline lineColor="">
+      <VerticalTimeline>
         {experiencesData?.map((item, index) => (
           <React.Fragment key={index}>
             <VerticalTimelineElement
