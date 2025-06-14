3a7208581bc2bf18a29f3a2fd252fb2a73e12490
devlopersabbir <devlopersabbir@gmail.com>
2025-06-14 20:20:24 +0600

    [blog post ]

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
-// @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
-import remarkToc from "remark-toc";
-import rehypePresetMinify from "rehype-preset-minify";
+import remarkGfm from "remark-gfm";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,16 +11,8 @@ export default defineConfig({
   },
 
   site: "https://devlopersabbir.github.io",
-  integrations: [
-    sitemap(),
-    mdx({
-      syntaxHighlight: "shiki",
-      shikiConfig: { theme: "dracula" },
-      remarkPlugins: [remarkToc],
-      rehypePlugins: [rehypePresetMinify],
-      remarkRehype: { footnoteLabel: "Footnotes" },
-      gfm: false,
-      optimize: true,
-    }),
-  ],
+  integrations: [sitemap(), mdx()],
+  markdown: {
+    remarkPlugins: [remarkGfm],
+  },
 });
---
+++ src/content/config.ts
@@ -0,0 +1,16 @@
+import { defineCollection, z } from "astro:content";
+
+const blogCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.date(),
+    author: z.string(),
+    tags: z.array(z.string()).optional(),
+    image: z.string(),
+  }),
+});
+export const collections = {
+  blogs: blogCollection,
+};
