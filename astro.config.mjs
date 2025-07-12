1ff46dd133366d694b070bb2aee0eca394baa4fc
devlopersabbir <devlopersabbir@gmail.com>
2025-07-13 00:29:34 +0600

    update actions with deno

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,13 @@ import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import remarkGfm from "remark-gfm";
 // import vercel from "@astrojs/vercel";
-import node from "@astrojs/node";
+// import node from "@astrojs/node";
 import react from "@astrojs/react";
-import vercelServerless from "@astrojs/vercel";
+// import vercelServerless from "@astrojs/vercel";
+import deno from "@deno/astro-adapter";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-
   vite: {
     plugins: [tailwindcss()],
   },
@@ -23,10 +22,6 @@ export default defineConfig({
     remarkPlugins: [remarkGfm],
   },
 
-  adapter: vercelServerless({
-    webAnalytics: {
-      enabled: true,
-    },
-    maxDuration: 8,
-  }),
+  output: "server",
+  adapter: deno(),
 });
