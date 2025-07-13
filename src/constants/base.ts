295da27b3ada1475063f95ef7c804d39b2194664
devlopersabbir <devlopersabbir@gmail.com>
2025-07-13 22:34:27 +0600

    added

--- src/constants/base.ts
+++ src/constants/base.ts
@@ -1,5 +1,5 @@
-export const baseUrl = import.meta.env.DEV
-  ? "http://localhost:5000"
-  : import.meta.env.SERVER_API_URL;
+// export const baseUrl = import.meta.env.DEV
+//   ? "http://localhost:5000"
+//   : import.meta.env.SERVER_API_URL;
 
-// /api/v1/mail/send
+export const baseUrl = "https://free-mail-sender.vercel.app";
