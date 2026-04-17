568934c114a81c4d15cef335f66a28d2eba53510
devlopersabbir <devlopersabbir@gmail.com>
2026-04-18 01:03:17 +0600

    feat: initialize Astro configuration with Tailwind, Sitemap, MDX, and React integrations

---
+++ astro.config.mjs
@@ -0,0 +1,21 @@
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
+import remarkGfm from "remark-gfm";
+import react from "@astrojs/react";
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
+export default defineConfig({
+    vite: {
+        plugins: [tailwindcss({})],
+        mode: "class",
+    },
+    site: "https://devlopersabbir.github.io",
+    integrations: [sitemap(), mdx(), react()],
+    markdown: {
+        remarkPlugins: [remarkGfm],
+    },
+});                                                               
