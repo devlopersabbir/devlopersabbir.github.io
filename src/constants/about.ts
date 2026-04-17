d5873a3cbc3258710c96868b919ae00be09212e1
devlopersabbir <devlopersabbir@gmail.com>
2026-04-18 02:13:32 +0600

    feat: add hero section, profile constants, global styles, and automated GitHub data fetching workflow

--- src/constants/about.ts
+++ src/constants/about.ts
@@ -1,7 +1,12 @@
 export const about = {
   name: "Sabbir Hossain Shuvo",
-  title: "Software Engineer",
-  description:
-    "Hi, this is Sabbir. A senior developer specialising in ReactJs, NextJs, NestJs, NodeJs, React-Native, and Python FastAPI framework. As a full-stack developer with over 4 years of experience in creating stunning WEB applications, BACKEND including Microservice API, & MOBILE apps for both ANDROID and IOS, with a passion for JavaScript, I can create amazing software solutions using only TypeScript & Python powerful language. So why settle for anything less? Contact me today and let's create something amazing together!",
+  title: "Software Engineer | Leadership  - API - DevOps - System Design",
+  description: `
+  I’m Sabbir, a backend-focused software engineer and engineering leader building scalable, production-grade systems used in real-world environments.
+  I specialize in designing and delivering high- performance backend architectures, microservice systems, and DevOps-driven infrastructure.My focus is simple: build systems that scale, perform, and don’t break under pressure.
+  Over the years, I’ve evolved from backend development into leading engineering teams, owning architecture decisions, and driving end - to - end delivery across complex products.I work across distributed systems, APIs, cloud infrastructure, and automation with a strong focus on reliability and system design.
+  My stack is not the identity engineering is.I use backend, DevOps, and system design principles to solve problems, not frameworks.
+  If a system needs to be fast, scalable, and production - ready that’s where I operate.
+  Let’s build something that actually performs.`,
   image: "/sabbir-profile.jpeg",
 } as const;
